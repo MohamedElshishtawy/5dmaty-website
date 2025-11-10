30a8dff7bb20f5b9f6f874723216410aaa384b4b
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-11-11 00:19:59 +0300

    Add debug statement to googleCallback method in SocialAuthController to inspect user data returned from Google authentication.

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -5,20 +5,32 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
 {
     public function  googleRedirect()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')
+        ->scopes(['openid', 'profile', 'email', 'https://www.googleapis.com/auth/user.phonenumbers.read'])
+        ->redirect();
     }
 
     public function googleCallback()
     {
         $user = Socialite::driver('google')->user();
 
-        dd($user);
+
+        $accessToken = $user->token;
+
+        $response = Http::withToken($accessToken)
+            ->get('https://people.googleapis.com/v1/people/me?personFields=phoneNumbers');
+
+        $phone = optional($response->json()['phoneNumbers'][0])['value'] ?? null;
+
+        dd($phone);
+        
         $oldUser = User::where('email', $user->email)->first();
 
         if ($oldUser) {
