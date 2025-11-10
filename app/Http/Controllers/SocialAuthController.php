f3f12045f73236c279d192c04e06f7f28c0bdd93
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-11-11 00:22:33 +0300

    Refactor Google authentication in SocialAuthController by removing phone number retrieval and updating user model to allow nullable phone field in migration.

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -5,32 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
 {
     public function  googleRedirect()
     {
-        return Socialite::driver('google')
-        ->scopes(['openid', 'profile', 'email', 'https://www.googleapis.com/auth/user.phonenumbers.read'])
-        ->redirect();
+        return Socialite::driver('google')->redirect();
     }
 
     public function googleCallback()
     {
         $user = Socialite::driver('google')->user();
 
-
-        $accessToken = $user->token;
-
-        $response = Http::withToken($accessToken)
-            ->get('https://people.googleapis.com/v1/people/me?personFields=phoneNumbers');
-
-        $phone = optional($response->json()['phoneNumbers'][0])['value'] ?? null;
-
-        dd($phone);
-        
         $oldUser = User::where('email', $user->email)->first();
 
         if ($oldUser) {
@@ -41,7 +28,6 @@ class SocialAuthController extends Controller
         $user = User::create([
             'name' => $user->name,
             'email' => $user->email,
-            'phone' => $user->phone,
             'password' => bcrypt('Password123345678'),
             'social_id' => $user->id,
         ]);
