80d0301c98d185be82aa84f0b0e0c3599cd9fc11
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2025-12-21 14:50:02 +0300

    Update global WhatsApp number format in constants configuration

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 
 return [
     // Global WhatsApp number used across the site (E.164 without +)
-    'whatsapp_number' => env('WHATSAPP_NUMBER', '201065189050'),
+    'whatsapp_number' => "01065189050",
 ];
 
 
