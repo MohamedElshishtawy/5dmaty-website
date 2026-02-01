911ec1776c3527e98243c625073d4099dfe496de
MohamedElshishtawy <mohamed.elshishtawy11@gmail.com>
2026-02-01 19:48:38 +0300

    Refactor middleware configuration to append AuthenticateSession and StartSession to the web group

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,10 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
-        $middleware->web([
-            AuthenticateSession::class,
-            StartSession::class,
-            
+        $middleware->appendToGroup('web', [
             \App\Http\Middleware\LangMiddleware::class,
         ]);
     })
